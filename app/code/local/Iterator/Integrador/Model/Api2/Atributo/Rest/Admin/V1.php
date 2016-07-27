fe2dea2ab2e72080fc02b833c5fe1fff3470f5fd
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2016-07-27 19:50:42 -0300

    Commit referente ao issue #2
    Criar métodos para gerenciar atributos pela REST API

---
+++ app/code/local/Iterator/Integrador/Model/Api2/Atributo/Rest/Admin/V1.php
@@ -0,0 +1,58 @@
+<?php
+ /**
+ * Iterator Sistemas Web
+ *
+ * NOTAS SOBRE LICENÇA
+ *
+ * Este arquivo de código-fonte está em vigência dentro dos termos da EULA.
+ * Ao fazer uso deste arquivo em seu produto, automaticamente você está 
+ * concordando com os termos do Contrato de Licença de Usuário Final(EULA)
+ * propostos pela empresa Iterator Sistemas Web.
+ *
+ * =================================================================
+ *                      MÓDULO INTEGRADOR REST
+ * =================================================================
+ * Este produto foi desenvolvido para o Ecommerce Magento de forma a
+ * possibilitar que sejam disponibilizados novos métodos para a REST
+ * API que por padrão não existem no Magento e se fazem necessários 
+ * para a integração com diversos ERP de mercado. 
+ * Através deste módulo a loja virtual do contratante do serviço
+ * passará a conter diversas novas funcionalidades para integrações
+ * via REST API do Magento com seus softwares ERP.
+ * =================================================================
+ *
+ * @category   Iterator
+ * @package    Iterator_Integrador
+ * @author     Ricardo Auler Barrientos <contato@iterator.com.br>
+ * @copyright  Copyright (c) Iterator Sistemas Web - CNPJ: 19.717.703/0001-63
+ * @license    O Produto é protegido por leis de direitos autorais, bem como outras leis de propriedade intelectual.
+ */
+
+class Iterator_Integrador_Model_Api2_Atributo_Rest_Admin_V1 extends Mage_Catalog_Model_Api2_Product_Rest {
+    
+    /**
+     * Busca o produto o atributo a partir do ID passado por parametro.
+     *
+     * @throws Mage_Api2_Exception
+     * @return array
+     */
+    protected function _retrieve() {
+        $attributeId = $this->getRequest()->getParam('attribute_id');
+        $attributeModel = Mage::getModel('eav/entity_attribute')->load($attributeId);
+        return $attributeModel->getData();
+    }
+    
+    /**
+     * Retorna lista com todos os atributos disponíveis.
+     *
+     * @throws Mage_Api2_Exception
+     * @return array
+     */
+    protected function _retrieveCollection() {
+        $attributes = Mage::getResourceModel('catalog/product_attribute_collection');
+        $data = $attributes->load()->toArray();
+        return $data['items'];
+    }
+}
+
+?>
