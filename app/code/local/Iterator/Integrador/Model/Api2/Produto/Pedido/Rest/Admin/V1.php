13270e29d75feca8393dc18b1744394d6aa20e05
Ricardo Auler Barrientos <desenvolvimento@iterator.com.br>
2016-08-22 20:57:43 -0300

    Commit referente ao issue #4
    Criar métodos para gerenciar produtos em pedidos pela REST API

---
+++ app/code/local/Iterator/Integrador/Model/Api2/Produto/Pedido/Rest/Admin/V1.php
@@ -0,0 +1,57 @@
+<?php
+ /**
+ * Iterator Sistemas Web
+ *
+ * NOTAS SOBRE LICENÇA
+ *
+ * Este arquivo de código-fonte está em vigência dentro dos termos da EULA.
+ * Ao fazer uso deste arquivo em seu produto, automaticamente você está 
+ * concordando com os termos do Contrato de Licença de Usuário Final(EULA)
+ * propostos pela empresa Iterator Sistemas Web.
+ *
+ * =================================================================
+ *                      MÓDULO INTEGRADOR REST
+ * =================================================================
+ * Este produto foi desenvolvido para o Ecommerce Magento de forma a
+ * possibilitar que sejam disponibilizados novos métodos para a REST
+ * API que por padrão não existem no Magento e se fazem necessários 
+ * para a integração com diversos ERP de mercado. 
+ * Através deste módulo a loja virtual do contratante do serviço
+ * passará a conter diversas novas funcionalidades para integrações
+ * via REST API do Magento com seus softwares ERP.
+ * =================================================================
+ *
+ * @category   Iterator
+ * @package    Iterator_Integrador
+ * @author     Ricardo Auler Barrientos <contato@iterator.com.br>
+ * @copyright  Copyright (c) Iterator Sistemas Web - CNPJ: 19.717.703/0001-63
+ * @license    O Produto é protegido por leis de direitos autorais, bem como outras leis de propriedade intelectual.
+ */
+
+class Iterator_Integrador_Model_Api2_Produto_Pedido_Rest_Admin_V1 extends Mage_Catalog_Model_Api2_Product_Rest {
+    
+    /**
+     * Busca os pedidos a partir do SKU do produto passado por parametro e retorna a quantidade deste produto reservada em pedidos pendentes.
+     *
+     * @throws Mage_Api2_Exception
+     * @return int
+     */
+    protected function _retrieve() {
+        $sku = $this->getRequest()->getParam('sku');
+        $totalReservado = 0;
+        $orderItemCollection = Mage::getModel('sales/order_item')->getCollection();
+        $orderItemCollection->getSelect()->group('main_table.item_id')
+                ->joinLeft(array('order' => $orderItemCollection->getTable('sales/order')),
+                'main_table.order_id = order.entity_id', 
+                array('prod_id' => 'main_table.product_id', 'order_date' => 'order.created_at', 'increment_id' => 'order.increment_id', 'status' => 'order.status'));
+        $orderItemCollection->addFieldToFilter('order.status', array('eq' => 'pending'));
+        $orderItemCollection->addFieldToFilter('main_table.sku', array('eq' => $sku));
+        foreach($orderItemCollection as $orderItem) {
+            $totalReservado += (int)$orderItem->getQtyOrdered();
+        }
+        
+        return $totalReservado;
+    }
+}
+
+?>
